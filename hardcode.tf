c4ca8a667db5766f84d5d69d77ab8a0917f74383
Njkvarma <njkvarma999@gmail.com>
2023-03-29 15:46:15 +0530

    initial commit

---
+++ hardcode.tf
@@ -0,0 +1,38 @@
+provider "azurerm" {
+    features{}
+}
+
+
+resource "azurerm_resource_group" "hcrg" {
+  name     = "hcrg1"
+  location = "West Europe"
+}
+
+resource "azurerm_network_security_group" "hcnsg" {
+  name                = "hcnsg1"
+  location            = azurerm_resource_group.hcrg.location
+  resource_group_name = azurerm_resource_group.hcrg.name
+}
+
+resource "azurerm_virtual_network" "hcvnet" {
+  name                = "hcvnet1"
+  location            = azurerm_resource_group.hcrg.location
+  resource_group_name = azurerm_resource_group.hcrg.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.hcnsg.id
+  }
+    tags = {
+    environment = "Production"
+  }
+}
